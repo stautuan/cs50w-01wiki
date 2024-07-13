50ecff1e1d5db2bc95af4086b9d135d1aec711a3
stautuan <stautuan89@gmail.com>
2024-07-13 14:41:16 -0400

    remove unused code

--- encyclopedia/models.py
+++ encyclopedia/models.py
@@ -1,9 +1,1 @@
 from django.db import models
-
-
-class Entry(models.Model):
-    title = models.CharField(max_length=200)
-    content = models.TextField()
-
-    def __str__(self):
-        return self.title
